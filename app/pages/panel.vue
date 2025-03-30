68a36d05001fef4c6e7e3fd5c1dbedd505f4f937
norbiros <norbiros@protonmail.com>
2025-03-30 16:52:55 +0200

    enhancement: Improve panel page

--- app/pages/panel.vue
+++ app/pages/panel.vue
@@ -6,6 +6,8 @@ definePageMeta({
 const { user, clear } = useUserSession()
 const { data } = await useFetch('/api/task/list')
 
+const today = new Date().toISOString().split('T')[0]
+
 function logout() {
   clear()
   navigateTo('/')
@@ -14,27 +16,44 @@ function logout() {
 
 <template>
   <div class="m-10 flex flex-col gap-5">
-    <h1 class="font-bold text-2xl flex items-center gap-2">
-      <img :src="user?.picture" alt="User profile picture" class="rounded-full h-8">
-      Witaj {{ user?.email }}!
-    </h1>
-
-    Miłego zgadywania słów!
-
-    <div class="grid grid-cols-7 gap-5">
-      <div v-for="(task, index) in data" :key="index" class="border border-gray-700 w-40 text-center">
-        <h2 class="bg-red-600">
-          {{ task.date }}
-        </h2>
-        <div>{{ task.solved ? 'Rozwiązane' : 'Nie rozwiązane' }}</div>
-        <UIcon name="i-material-symbols-crown" :class="task.solved ? 'text-yellow-500' : 'text-gray-500'" />
+    <header class="flex items-center justify-between">
+      <div class="flex items-center gap-2">
+        <h1 class="font-bold text-2xl">
+          Witaj {{ user?.email }} w Kingdomdle!
+        </h1>
       </div>
-    </div>
-
-    <div>
-      <UButton @click="logout">
+      <UButton class="bg-red-600 hover:bg-red-700 text-white" @click="logout">
         Wyloguj
       </UButton>
+    </header>
+
+    <p class="text-lg font-medium">
+      Miłego zgadywania słów!
+    </p>
+
+    <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-7 gap-5">
+      <div
+        v-for="(task, index) in data"
+        :key="index"
+        class="border rounded-lg shadow-lg" :class="[
+          task.date === today ? 'border-red-500' : 'border-gray-700',
+        ]"
+      >
+        <h2 class="bg-red-600 text-white p-2 rounded-t-lg">
+          {{ task.date }}
+        </h2>
+        <div class="p-3 flex flex-wrap justify-center items-center gap-2">
+          <UIcon
+            name="i-material-symbols-crown"
+            :class="task.solved ? 'text-yellow-500 text-xl' : 'text-gray-500 text-xl'"
+          />
+          {{ task.solved ? 'Rozwiązane' : 'Nie rozwiązane' }}
+          <br>
+          <UButton v-if="task.date === today && !task.solved" to="/game">
+            Rozwiąż
+          </UButton>
+        </div>
+      </div>
     </div>
   </div>
 </template>
